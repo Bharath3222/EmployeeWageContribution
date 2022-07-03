55aeaa44d8dd46db8e4d1bfa2c2e74c3cbd76dda
Bharath3222 <bharat322.korada@gmail.com>
2022-07-03 09:15:42 +0530

    Full time wage calucaltion

--- EmployeeWageContribution.java
+++ EmployeeWageContribution.java
@@ -3,19 +3,22 @@ package org.bridge.git;
 import java.util.Random;
 
 public class EmployeeWageContribution {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Random random= new Random();
-		int empAttendance=random.nextInt(2);
-		if(empAttendance==0)
-		{
-			System.out.println("Employee is Absent");
-		}
-		else
-		{
-			System.out.println("Employee is Present");
-		}
+		
+
+	final int WAGE_PER_HOUR = 20;	
+	Random random= new Random();
+	int isEmpPresent = random.nextInt(2);
+	int empHR;
+	if (isEmpPresent == 1) {
+		System.out.println("Employee is present");
+		empHR = 8;
+	} else {
+		System.out.println("Employee is absent");
+		empHR = 0;
+	}
+	int empWage = empHR * WAGE_PER_HOUR;
+	System.out.println("Employee Wage is : "+empWage);
 	}
 
 }
