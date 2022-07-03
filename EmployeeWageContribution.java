d28d943a65a91ee6abeabc7ee636955d4bdc212b
Bharath3222 <bharat322.korada@gmail.com>
2022-07-03 09:22:14 +0530

    part time added

--- EmployeeWageContribution.java
+++ EmployeeWageContribution.java
@@ -5,20 +5,23 @@ import java.util.Random;
 public class EmployeeWageContribution {
 	public static void main(String[] args) {
 		
-
-	final int WAGE_PER_HOUR = 20;	
-	Random random= new Random();
-	int isEmpPresent = random.nextInt(2);
-	int empHR;
-	if (isEmpPresent == 1) {
-		System.out.println("Employee is present");
-		empHR = 8;
-	} else {
-		System.out.println("Employee is absent");
-		empHR = 0;
-	}
-	int empWage = empHR * WAGE_PER_HOUR;
-	System.out.println("Employee Wage is : "+empWage);
+		final int WAGE_PER_HOUR = 20;	
+		Random random= new Random();
+		int isEmpPresent = random.nextInt(3);
+		int empHR;
+		if (isEmpPresent == 1) {
+			System.out.println("Employee is present");
+			empHR = 8;
+		} else if(isEmpPresent == 0){
+			System.out.println("Employee is absent");
+			empHR = 0;
+		}
+		else {
+			System.out.println("Emp[loyee is working as PartTime");
+			empHR=4;
+		}
+		int empWage = empHR * WAGE_PER_HOUR;
+		System.out.println("Employee Wage is : "+empWage);
 	}
 
 }
